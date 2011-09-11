4f3fe67e6dde11697c27cc7ec1fd724a3596638a
Thomas Recloux <thomas.recloux@gmail.com>
2011-09-11 15:49:43 +0200

    Changing password

---
+++ app/controllers/Account.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import models.User;
+import play.data.validation.Equals;
+import play.data.validation.MinSize;
+import play.data.validation.Required;
+import play.mvc.Controller;
+import play.mvc.With;
+
+@With(Secure.class)
+public class Account extends Controller {
+	
+	public static void changePasswordForm() {
+		render();
+	}
+	
+	public static void performChangePassword(
+			@Required String oldPassword, 
+			@Required @MinSize(6) @Equals("newPasswordConfirm") String newPassword, 
+			@Required @MinSize(6) @Equals("newPassword") String newPasswordConfirm) {
+
+		User user = User.findByEmail(Security.connected());
+		
+		if (oldPassword != null && !user.authenticate(oldPassword)) {
+			validation.addError("oldPassword", "invalid password");
+		}
+		
+		if (validation.hasErrors()) {
+			params.flash(); // add http parameters to the flash scope
+			validation.keep(); // keep the errors for the next request
+			changePasswordForm();
+		}
+		
+		user.password = newPassword;
+		user.hashPassword();
+		user.save();
+		
+		flash.success("Password successfully changed");
+		Home.home();
+		
+	}
+
+}
