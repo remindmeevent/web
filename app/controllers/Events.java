56755948d27f63134585de073e463f2cbefe4132
Thomas Recloux <thomas.recloux@gmail.com>
2011-09-11 14:47:07 +0200

    Checking event owner

--- app/controllers/Events.java
+++ app/controllers/Events.java
@@ -1,13 +1,23 @@
 package controllers;
 
 import models.Event;
-import play.db.jpa.JPABase;
 import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.With;
 
+@With(Secure.class)
 public class Events extends Controller{
 
 	public static void show(Long id) {
 		Event event = Event.findById(id);
-		render(event);
+		if (eventBelongsToConnectedUser(event)) {
+			render(event);
+		} else {
+			error(Http.StatusCode.FORBIDDEN, "You do not own this event");
+		}
+	}
+
+	private static boolean eventBelongsToConnectedUser(Event event) {
+		return event.user.email.equals(Security.connected());
 	}
 }
